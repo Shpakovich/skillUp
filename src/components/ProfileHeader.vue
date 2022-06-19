57b67452a16b68e37dff7b719e796b09bfe2d638
Shpakovich <shmakovbx@gmail.com>
2022-06-19 20:34:18 +0300

    main add profile page with header

---
+++ src/components/ProfileHeader.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="header-block">
+        <div class="avatar-block">
+            <div class="avatar"></div>
+            <div class="name-block">
+                <p>Denis Shmakov</p>
+                <p>Front-end Developer</p>
+            </div>
+        </div>
+        <div class="points-block">
+            <p class="points">66/74 Points</p>
+            <p class="level">4-s level</p>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "ProfileHeader"
+    }
+</script>
+
+<style scoped>
+    .header-block {
+        padding: 16px;
+    }
+    .avatar-block {
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
+        margin-bottom: 8px;
+    }
+    .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-image: url("../assets/images/warcraft-avatar.jpg");
+        background-size: cover;
+        background-position: center;
+    }
+    .name-block {
+        display: flex;
+        flex-direction: column;
+    }
+    .points-block {
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
+    }
+</style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,23 @@
+<template>
+    <ProfileHeader class="header" />
+</template>
+
+<script>
+    import ProfileHeader from '../components/ProfileHeader.vue'
+    export default {
+        name: "ProfileView",
+        components: {
+            ProfileHeader
+        }
+    }
+</script>
+
+<style scoped>
+    .header {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: rgba(23, 52, 118, 0.3);
+    }
+</style>
